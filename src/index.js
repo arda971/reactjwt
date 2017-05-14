97102321cebae67515a836ed27d970873e49ea8f
arda971 <pyc971@gmail.com>
2017-05-14 16:27:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './components/Login';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Login />,
   document.getElementById('root')
 );
