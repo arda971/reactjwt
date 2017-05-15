2d8068fd20f0f44725529d5eff05c28c7e4d6e82
arda971 <pyc971@gmail.com>
2017-05-15 00:56:12 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import Nav from './components/Nav';
+import Login from './components/Login';
+
+
+
+
+ReactDOM.render(
+  <Nav/>,
+  document.getElementById('root')
+);
