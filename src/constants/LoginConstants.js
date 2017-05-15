c68eb606726642ad9cda679c5c0bc6e045bb2020
arda971 <pyc971@gmail.com>
2017-05-15 14:15:16 -0400

    Delete LoginConstants.js

--- src/constants/LoginConstants.js
+++
@@ -1,8 +0,0 @@
-var BASE_URL = 'https://i2x-challenge.herokuapp.com/core/login/';
-export default {
-  BASE_URL: BASE_URL,
-  LOGIN_URL: BASE_URL,
-  SIGNUP_URL: BASE_URL + 'users',
-  LOGIN_USER: 'LOGIN_USER',
-  LOGOUT_USER: 'LOGOUT_USER'
-}
